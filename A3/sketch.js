6b0b795e2e583f578483818322aa922258d2b16d
sanjanarattan <rsanju191@gmail.com>
2025-02-10 19:38:21 -0800

    Update boxplot logic

--- A3/sketch.js
+++ A3/sketch.js
@@ -2,6 +2,7 @@ let gx = 10;
 let gy = 10;
 let hx = gx;
 let hy = gy + 140;
+let by = hy + 2000;
 let slider;
 let f1;
 
@@ -16,8 +17,10 @@ function preload(){
   //table = loadTable('./data/cod2.csv', 'csv', 'header')
   table = loadTable(gist, 'csv', 'header')
   // TODO maybe put in gist
-  f1 = loadImage("./images/Figure_1.png")
-  bg = loadImage("./images/dotted_notebook.png")
+  f1 = loadImage("./images/Figure_1.png");
+  bg = loadImage("./images/dn.png");
+  f2 = loadImage("./images/Figure_6.png");
+
 
 }
 
@@ -55,8 +58,6 @@ function subtitle(x, y, z){
   text(z, x, y);
   pop();
 }
-
-
 
 
 // Histogram - ROD freqs
@@ -142,6 +143,7 @@ function hist(y){
     text((i * max_freq_label) / 10, hx + 40, y_start);
     line(hx + 58, y_start, hx + 60, y_start);
     y_start = y_start- y_ticks_scale
+    pop()
   }
   //baars
   let bar_x = hx + 60;
@@ -151,8 +153,8 @@ function hist(y){
     push();
     fill("pink");
     rect(bar_x, hy + 500 -bar_height, bar_width, bar_height);
-    pop();
-    bar_x = bar_x+bar_width;  
+    bar_x = bar_x+bar_width; 
+    pop() 
   }
   // kernel density
   let smoothed_values =[]
@@ -184,13 +186,31 @@ function hist(y){
   pop()
 }
 
+function s_hist(){
+  push();
+  textSize(17)
+  text("2. Distribution of RODS in the USA (Seaborn Version)", hx, hy + 650);
+  image(f1, hx, hy + 700, hx+ 1100);
+  pop();
+}
+
+function s_bw(){
+  push();
+  textSize(17)
+  text("4. Distribution of RODS in the USA (Seaborn Version)", hx, by + 300);
+  image(f2, hx, hy + 2400, hx+ 1100);
+  pop();
+}
+
+
+
 // box whiker 
 function bw(y){
   // graph title
   push();
   fill(0);
   textSize(17);
-  text(`2. Distribution of Age Related Rate of Death due to Cerebrovascular Diseases in the USA (${y})`, hx, hy);
+  text(`3. Distribution of Age Related Rate of Death due to Cerebrovascular Diseases in the USA (${y})`, hx, by - 250);
   pop();
 
   let year = `${y}`;
@@ -198,23 +218,86 @@ function bw(y){
   //console.log(data_fi)
 
   
-  data_fi.sort((a, b) => b.rate - a.rate); 
-  console.log(data_fi)
+  data_fi.sort((a, b) => a.rate - b.rate); 
+  //console.log(data_fi)
 
+  let median = (data_fi[24].rate + data_fi[25].rate) / 2;
+  let q1 = ((data_fi[12].rate + data_fi[13].rate)) / 2;  
+  let q3 = ((data_fi[36].rate + data_fi[37].rate)) / 2; 
+  let iqr = Math.abs(q1 - q3);
+  let min = q1 - (1.5 * iqr);
+  let max = q3 + (1.5 * iqr);
 
-  // we assume 50 states because data is clean
-  let mid = 25; 
-  let median = (data_fi[mid - 1].rate + data_fi[mid].rate) / 2; 
-  let q1 = data_fi[Math.floor(mid / 2)].rate; 
-  let q3 = data_fi[mid + Math.floor(mid / 2)].rate; 
-  let iqr = q3 - q1
-  let min = q1 - (1.5 * iqr)
-  let max = q3 + (1.5 * iqr)
-
-  console.log("median:", median, "q1:", q1, "q3:", q3, "min:", min, "max", max); 
+  //console.log("median:", median, "q1:", q1, "q3:", q3, "min:", min, "max", max);
   
+  let x_start = gx + 60;
+  let x_end = x_start + 1000;
+  let box_height = 100;
+
+  push();
+  stroke(0);
+  line(x_start, by + box_height + 20, x_end, by + box_height + 20);
+  pop();
+
+  let tick_count = 6; 
+  let x_ticks_scale = (x_end - x_start) / (tick_count - 1);
+
+  push();
+  textSize(14);
+  text("Rate of Death", ((x_start + x_end) / 2) - 40, by + box_height + 80);
+  pop();
+
+  let labels = ["0", "20", "40", "60", "80", "100"]
+  for (let i = 0; i < tick_count; i++) {
+    push();
+    fill(0);
+    textSize(10);
+    text(labels[i], x_start + i * x_ticks_scale, by + box_height + 30);
+    line(x_start + i * x_ticks_scale, by + box_height + 20 , x_start + i * x_ticks_scale, by + box_height + 25);
+    pop();
+  }
+
+
+
+  let x_min = map(min, 0, 100, x_start, x_end); 
+  let x_q1 = map(q1, 0, 100, x_start, x_end);   
+  ellipse(x_min, by - 100, 1.5);
+  ellipse(x_q1, by - 100, 1.5);
+  line(x_min, by - 100, x_q1, by - 100);
+
+  let x_max = map(max, 0, 100, x_start, x_end); 
+  let x_q3 = map(q3, 0, 100, x_start, x_end);   
+  ellipse(x_max, by - 100, 1.5);
+  ellipse(x_q3, by - 100, 1.5);
+  line(x_q3, by - 100, x_max, by - 100);
+
+  push();
+  fill(230, 230, 250);
+  rect(x_q1, by - 200, x_q3 - x_q1 , 200);
+  pop();
+
+  let x_med = map(median, 0, 100, x_start, x_end); 
+  ellipse(x_med, by - 100, 1.5);
+  line(x_med, by - 200, x_med, by);
+
+
+ 
+  let arr = data_fi.map(d => d.rate);
+
+  for (let d of arr) {
+    if (d < min || d > max) {
+      //console.log(d);  
+      let o_scale = map(d, 0, 100, x_start, x_end); 
+      push();
+      noFill();
+      ellipse(o_scale, by - 100, 5);  
+      pop();
+    }
 }
 
+
+
+}
 
 function setup() {
   createCanvas(1600, 5000);
@@ -222,19 +305,23 @@ function setup() {
   background(255);
   slider = createSlider(1999, 2017, 1999, 1);
   slider.position(hx + 1100, hy + 500);
-  slider.size(50)
-  read_data()
-  bw(1999)
+  slider.size(50);
+
+  slider2 = createSlider(1999, 2017, 1999, 1);
+  slider2.position(hx + 1100, by + 100);
+  slider2.size(50);
+
+  read_data();
 }
 
 function draw() {
-  background(256)
-  image(bg, 0, 0, 1600, 10000)
+  background(256);
+  image(bg, 0, 0, 1600, 10000);
   title();
   let y = slider.value();
   hist(y);
-  textSize(15)
-  text("2. Distribution of RODS in the USA (Seaborn Version)", hx, hy + 600);
-  image(f1, hx, hy + 700, hx+ 1000, hy + 800);
-  
+  let z = slider2.value();
+  bw(z);  
+  s_hist();
+  s_bw();
 }
